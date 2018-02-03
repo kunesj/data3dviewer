60c2785f557efacde4fcd1ef888113bddaa24570
Jiří Kuneš <jirka642@gmail.com>
2018-02-03 16:09:16 +0100

    fix for io3d <-512;511> value range bug

--- data3dviewer/__main__.py
+++ data3dviewer/__main__.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 import sys, os, argparse
 
+import numpy as np
 import io3d
 import sed3
 
@@ -34,6 +35,10 @@ def main():
     logger.info("Loading 3d data from: %s" % os.path.abspath(args.path))
     datap = io3d.read(os.path.abspath(args.path), dataplus_format=True)
     data3d = datap["data3d"]
+    
+    # temporary fix for io3d <-512;511> value range bug
+    # this is caused by hardcoded slope 0.5 in dcmreader
+    if np.min(data3d) >= -512: data3d = data3d * 2
 
     # display data
     ed = sed3.sed3(data3d)
